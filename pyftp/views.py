6150e49cf43a29523ef821d5dbc2cb7741be3947
soltanoff <piccadillable@gmail.com>
2017-09-08 21:13:48 +0300

    Added views

---
+++ pyftp/views.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_protect
+
+from pyftp.models import FileModel
+
+
+@csrf_protect
+@login_required
+def index(request):
+    search_param = request.GET.get('q', '')
+    if search_param:
+        files = FileModel.objects.filter(file_name__contains=search_param)
+    else:
+        files = FileModel.objects.all()
+
+    return render(
+        request,
+        'pyftp/index.html',
+        {
+            'files': files,
+            'search': search_param,
+            'file_removed': request.file_removed if hasattr(request, 'file_removed') else '',
+            'file_uploaded': request.file_uploaded if hasattr(request, 'file_uploaded') else []
+        }
+    )
+
+
+@csrf_protect
+@login_required
+def upload(request):
+    file = request.FILES.get('file', None)
+    file_name = request.POST.get('file_name', '')
+    notes = request.POST.get('notes', '')
+
+    if file and file_name and notes:
+        file_model = FileModel(file_name=file_name, notes=notes, file=file)
+        file_model.save()
+
+        request.file_uploaded = [file_model.id, file_model.file_name]
+        return index(request)
+
+    return render(
+        request,
+        'pyftp/upload.html',
+        {
+            'errors': bool(file_name or notes or file),
+            'file': file,
+            'file_name': file_name,
+            'notes': notes
+        }
+    )
+
+
+@csrf_protect
+@login_required
+def remove(request, file_id):
+    if request.user.is_active and request.user.is_staff:
+        file_model = FileModel.objects.filter(id=file_id)
+        if len(file_model):
+            request.file_removed = "%s (%s)" % (file_model[0].file_name, file_model[0].file.name)
+
+        file_model.delete()
+        return index(request)
+    else:
+        raise Http404
+
+
+@csrf_protect
+def login(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    error_msg = False
+
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect("/")
+
+    if username and password and user is None:
+        error_msg = True
+
+    return render(request, 'pyftp/login.html', {'error': error_msg})
+
+
+@csrf_protect
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect("/accounts/login/")
+
+
+@csrf_protect
+@login_required
+def download_file(request, file_id):
+    if request.user.is_active:
+        obj = get_object_or_404(FileModel, pk=file_id)
+        obj.count += 1
+        obj.save()
+
+        response = HttpResponse(obj.file, content_type='text/plain')
+        response['Content-Disposition'] = 'attachment; filename="%s"' % obj.file.name
+
+        return response
+    else:
+        raise Http404
