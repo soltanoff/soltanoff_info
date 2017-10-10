3cd54a3f6ad2baff0d93350fdf2822371f12233e
soltanoff <piccadillable@gmail.com>
2017-10-11 02:39:48 +0300

    changed db: sqlite3 -> mysql

--- django_srv/settings.py
+++ django_srv/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_srv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pyftp_db',
+        'USER': 'root',
+        'PASSWORD': 'dbpassword',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
