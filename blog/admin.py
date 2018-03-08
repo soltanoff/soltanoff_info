582cc51ebbbe2a8b317114e919ed475c723cf16e
soltanoff <piccadillable@gmail.com>
2018-03-08 23:23:37 +0300

    added the Post model for manage menu

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.contrib import admin
 
+from blog.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    list_display = ('title', 'datetime')
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Date information', {'fields': ['datetime'], 'classes': ['expand']}),
+        ('Content', {'fields': ['content'], 'classes': ['expand']})
+    ]
+
+
+admin.site.register(Post, PostAdmin)
