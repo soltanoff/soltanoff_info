5e0321cc55319923c6d23262b5358e55213735d0
soltanoff <piccadillable@gmail.com>
2018-03-09 01:34:14 +0300

    changed DEBUG param -> False

--- django_srv/settings.py
+++ django_srv/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '90)228yq1(j%px#zzqxn_c=-+qivz46vn_=-7hwvr%qb@&va^x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
