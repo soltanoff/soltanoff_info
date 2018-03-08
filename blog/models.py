20c04e1a45c21c8a1b1473e8f87e896dde97d765
soltanoff <piccadillable@gmail.com>
2018-03-08 23:23:08 +0300

    added the Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from datetime import datetime
 
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    datetime = models.DateTimeField('Publication date', default=datetime.now)
+    content = models.TextField(max_length=10000)
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/post/%i/" % self.id
