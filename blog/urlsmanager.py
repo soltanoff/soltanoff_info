52e03682a1740ba6734560931c4cc38fb2ac58f9
soltanoff <piccadillable@gmail.com>
2018-03-15 22:01:16 +0300

    upd url patterns

--- blog/urlsmanager.py
+++ blog/urlsmanager.py
@@ -1,9 +1,13 @@
+from django.conf.urls import url
 from django.urls import path
 
 from blog.apps import BlogConfig
+from blog.views import PostDetailView
 from . import views
 
 app_name = BlogConfig.name
 urlpatterns = [
     path(r'', views.index, name='index'),
+    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view()),
+    path(r'post/upload', views.upload, name='upload'),
 ]
