e36b03691659451839b68927c4798ddb0d661ef8
soltanoff <piccadillable@gmail.com>
2018-03-15 22:00:46 +0300

    changed name Post -> PostModel, added get methods

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,11 @@ from datetime import datetime
 from django.db import models
 
 
-class Post(models.Model):
+class PostModel(models.Model):
     title = models.CharField(max_length=255)
     datetime = models.DateTimeField('Publication date', default=datetime.now)
-    content = models.TextField(max_length=10000)
+    entry = models.TextField(max_length=2000, default='')
+    content = models.TextField(max_length=100000)
 
     def __unicode__(self):
         return self.title
@@ -15,5 +16,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return "/post/%i/" % self.id
+    def getUrl(self):
+        return '/post/%s' % self.id
+
+    def getAllContent(self):
+        return ''.join(map(lambda x: '%s' % str(x), (self.entry, self.content)))
