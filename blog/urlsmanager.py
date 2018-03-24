668f40348f9d49a8285e57b32cfc2867a52a1d95
soltanoff <piccadillable@gmail.com>
2018-03-24 23:23:37 +0300

    upd the urls

--- blog/urlsmanager.py
+++ blog/urlsmanager.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import PostDetailView
-from . import views
+from blog.views import PostDetailView, PostListView, PostCreateView
 
 app_name = BlogConfig.name
 urlpatterns = [
-    path(r'', views.index, name='index'),
-    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view()),
-    path(r'post/upload', views.upload, name='upload'),
+    path(r'', PostListView.as_view(), name='index'),
+    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post'),
+    path(r'post/upload', login_required(PostCreateView.as_view()), name='upload'),
 ]
