18b1eca446a7d70b62c7b011d856176f219fb6a8
Ilya <piccadillable@gmail.com>
2018-03-25 23:13:06 +0300

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 
 
+# TODO: need to the refactoring form
 class PostForm(forms.Form):
     title = forms.CharField(label="Title:", required=True)
     entry = forms.CharField(label="Entry:", required=True, widget=forms.Textarea)
