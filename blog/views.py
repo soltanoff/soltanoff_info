9828e74cef99aef078b32f1faaceb634120a6036
soltanoff <piccadillable@gmail.com>
2018-03-27 20:30:45 +0300

    fixed the searching, added the contrib.message using

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import DetailView, ListView, CreateView
 
 from blog.forms import PostForm
@@ -24,15 +25,27 @@ class PostListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(PostListView, self).get_context_data(**kwargs)
         context['search'] = self.request.GET.get('q', '')
+
+        if context['search']:
+            # TODO: сделать поиск по нескольким полям в стиле SQL-конструкции `LIKE`
+            self.queryset = PostModel.objects.filter(title__icontains=context['search'])
+        else:
+            self.queryset = PostModel.objects.all()
+        context[self.context_object_name] = self.queryset
         return context
 
 
-class PostCreateView(CreateView):
+class PostCreateView(SuccessMessageMixin, CreateView):
     template_name = "blog/upload.html"
     model = PostModel
     form = None
     fields = ["title", "entry", "content"]
     success_url = "../"
+    success_message = u"Article \"<a href=\"{href}\">{title}</a>\" created!"
+
+    def get_success_message(self, cleaned_data):
+        post = PostModel.objects.get(**cleaned_data)
+        return self.success_message.format(href=post.getUrl(), title=cleaned_data['title'])
 
     def get_context_data(self, **kwargs):
         context = super(PostCreateView, self).get_context_data(**kwargs)
