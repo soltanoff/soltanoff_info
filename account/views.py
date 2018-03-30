a3730a70dd964775193885f0ead222506ea18603
Ilya <piccadillable@gmail.com>
2018-03-30 18:53:34 +0300

    create CustomLoginView

--- account/views.py
+++ account/views.py
@@ -1,29 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.contrib import auth
+from django.contrib.auth.views import LoginView
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 
+from account.apps import AccountConfig
 
-@csrf_protect
-def login(request):
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
-    redirect_url = request.GET.get('next', '/')
-    error_msg = False
-    user = auth.authenticate(username=username, password=password)
 
-    if user is not None and user.is_active:
-        auth.login(request, user)
-        return HttpResponseRedirect(redirect_url)
+class CustomLoginView(LoginView):
+    template_name = "account/login.html"
+    redirect_authenticated_user = True
 
-    if username and password and user is None:
-        error_msg = True
-
-    return render(request, 'account/login.html', {'error': error_msg, 'url': redirect_url})
+    def get_redirect_url(self):
+        redirect_to = super(CustomLoginView, self).get_redirect_url()
+        return redirect_to if redirect_to else AccountConfig.default_redirect_url
 
 
 @csrf_protect
 def logout(request):
     auth.logout(request)
-    return HttpResponseRedirect("/")
+    return HttpResponseRedirect(AccountConfig.default_redirect_url)
