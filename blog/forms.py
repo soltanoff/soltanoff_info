05b2159e979457cf31c0bb2b0b4fdf0cd92dd605
Ilya <piccadillable@gmail.com>
2018-03-30 21:29:48 +0300

    changed the content widget if Form

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
 
 
@@ -6,4 +7,4 @@ from django import forms
 class PostForm(forms.Form):
     title = forms.CharField(label="Title:", required=True)
     entry = forms.CharField(label="Entry:", required=True, widget=forms.Textarea)
-    content = forms.CharField(label="Content:", required=True, widget=forms.Textarea)
+    content = forms.CharField(label="Content:", required=True, widget=CKEditorUploadingWidget())
