4b91c3df4d89eeb27732a4190dad79cd86a7d4b7
Ilya <piccadillable@gmail.com>
2018-03-30 22:02:48 +0300

    changed the content widget in Form

--- blog/forms.py
+++ blog/forms.py
@@ -6,5 +6,5 @@ from django import forms
 # TODO: need to the refactoring form
 class PostForm(forms.Form):
     title = forms.CharField(label="Title:", required=True)
-    entry = forms.CharField(label="Entry:", required=True, widget=forms.Textarea)
+    entry = forms.CharField(label="Entry:", required=True, widget=CKEditorUploadingWidget(config_name='simple_toolbar'))
     content = forms.CharField(label="Content:", required=True, widget=CKEditorUploadingWidget())
