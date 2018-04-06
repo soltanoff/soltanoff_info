1a8770398d189d973a5e6d45d02b17469af40054
soltanoff <piccadillable@gmail.com>
2018-04-06 23:11:33 +0300

    removed TODO

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,6 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 
-# TODO: need to the refactoring form
 class PostForm(forms.Form):
     title = forms.CharField(label=_("Title:"), required=True)
     entry = forms.CharField(label=_("Entry:"), required=True, widget=CKEditorUploadingWidget(config_name='simple_toolbar'))
