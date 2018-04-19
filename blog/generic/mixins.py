61e4b1b0bb99170a88c47b64609e3e447643a7b3
soltanoff <piccadillable@gmail.com>
2018-04-19 16:37:27 +0300

    add visible article tags in page + fix the base page UI

--- blog/generic/mixins.py
+++ blog/generic/mixins.py
@@ -7,4 +7,7 @@ class TagMixin(ContextMixin):
     def get_context_data(self, **kwargs):
         context = super(TagMixin, self).get_context_data(**kwargs)
         context['tags'] = TagModel.objects.all()
+        if hasattr(self, 'request'):
+            tag_id = self.request.GET.get('tag', None)
+            context['current_tags'] = TagModel.objects.get(pk=tag_id) if tag_id else None
         return context
