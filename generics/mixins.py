0d6f01c706548f534e3ab1f1c024a151e66c9032
soltanoff <piccadillable@gmail.com>
2018-05-05 00:39:55 +0300

    create new methods for QueryMixin

--- generics/mixins.py
+++ generics/mixins.py
@@ -1,5 +1,7 @@
+from django.db.models import Q
 from django.views.generic.base import ContextMixin
 from django.views.generic.list import MultipleObjectMixin
+from functools import reduce
 
 
 class SearchMixin(ContextMixin):
@@ -9,11 +11,29 @@ class SearchMixin(ContextMixin):
         return context
 
 
-# TODO: polcracker: try to change ORM
 class QueryMixin(MultipleObjectMixin):
-    # TODO: soltanoff: сделать поиск по нескольким полям в стиле SQL-конструкции `LIKE`
-    def _queryset_filter(self, **kwargs):
+    def _queryset_filter(self, *args, **kwargs):
         if not self.queryset:
-            return self.model.objects.filter(**kwargs)
+            return self.model.objects.filter(self._join_and(*args, **kwargs))
         else:
-            return self.queryset.filter(**kwargs)
+            return self.queryset.filter(self._join_and(*args, **kwargs))
+
+    def _join_or(self, *args, **kwargs):
+        result = []
+        if args:
+            result.append(reduce(lambda x, y: Q(x) | Q(y), args))
+        if kwargs and len(kwargs.keys()) > 1:
+            result.append(reduce(lambda x, y: Q(**{x: kwargs[x]} if x in kwargs else x) | Q(**{y: kwargs[y]}), kwargs))
+        else:
+            result.append(Q(**kwargs))
+        return reduce(lambda x, y: Q(x) | Q(y), filter(bool, result), Q())
+
+    def _join_and(self, *args, **kwargs):
+        result = []
+        if args:
+            result.append(reduce(lambda x, y: Q(x) & Q(y), args))
+        if kwargs and len(kwargs.keys()) > 1:
+            result.append(reduce(lambda x, y: Q(**{x: kwargs[x]} if x in kwargs else x) & Q(**{y: kwargs[y]}), kwargs))
+        else:
+            result.append(Q(**kwargs))
+        return reduce(lambda x, y: Q(x) & Q(y), filter(bool, result), Q())
