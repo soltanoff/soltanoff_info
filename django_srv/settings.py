c316c4f90e1551ed31f4aa5339e71f02b2860f79
soltanoff <piccadillable@gmail.com>
2018-05-04 22:01:50 +0300

    changed media root directory

--- django_srv/settings.py
+++ django_srv/settings.py
@@ -186,7 +186,7 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "files", "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media", "files")
 
 MEDIA_URL = "/media/"
 
