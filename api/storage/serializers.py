8babdac94c0b5b891090f93471a94ffdfd18713d
soltanoff <piccadillable@gmail.com>
2018-09-01 20:48:09 +0300

    changed type of `file` field, changed rules for `count` field, added download link (storage.views.download_file())

--- api/storage/serializers.py
+++ api/storage/serializers.py
@@ -4,12 +4,14 @@ from storage.models import FileModel
 
 
 class FileModelSerializer(serializers.HyperlinkedModelSerializer):
-    file = serializers.SerializerMethodField('get_file_uri')
+    file = serializers.FileField(write_only=True)
     file_name = serializers.SerializerMethodField()
+    download_link = serializers.SerializerMethodField('get_file_uri')
 
     class Meta:
         model = FileModel
-        fields = ('url', 'title', 'notes', 'upload_date', 'count', 'file', 'file_name')
+        fields = ('url', 'title', 'notes', 'upload_date', 'count', 'file', 'file_name', 'download_link')
+        read_only_fields = ('count', )
 
     def get_file_uri(self, obj):
         request = self.context.get('request')
