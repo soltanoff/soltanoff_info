a85fe671e709cdf461dd7cf2bcdef9169f744841
soltanoff <piccadillable@gmail.com>
2018-09-01 19:18:10 +0300

    get absolute uri of the file downloading

--- api/storage/serializers.py
+++ api/storage/serializers.py
@@ -4,15 +4,16 @@ from storage.models import FileModel
 
 
 class FileModelSerializer(serializers.HyperlinkedModelSerializer):
-    file = serializers.SerializerMethodField()
+    file = serializers.SerializerMethodField('get_file_uri')
     file_name = serializers.SerializerMethodField()
 
     class Meta:
         model = FileModel
-        fields = ('title', 'notes', 'upload_date', 'count', 'file')
+        fields = ('title', 'notes', 'upload_date', 'count', 'file', 'file_name')
 
-    def get_file(self, obj):
-        return obj.get_url()
+    def get_file_uri(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.get_url())
 
     def get_file_name(self, obj):
         return obj.file_name
