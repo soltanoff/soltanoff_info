03ef53f9d8c7d757e49789c08451cd2a54a6e5c2
soltanoff <piccadillable@gmail.com>
2018-09-01 21:09:24 +0300

    added `group_names` field :boom:

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -5,10 +5,11 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     is_staff = serializers.HyperlinkedIdentityField(view_name='user-staff', format='html')
     groups = serializers.HyperlinkedRelatedField(many=True, view_name='group-detail', read_only=True)
+    group_names = serializers.HyperlinkedIdentityField(view_name='user-group-names', read_only=True)
 
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups', 'is_staff', 'is_superuser')
+        fields = ('url', 'username', 'email', 'groups', 'group_names', 'is_staff', 'is_superuser')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
