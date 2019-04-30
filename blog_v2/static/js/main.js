a5b29f699eaa0ea1c6fc88d2772ca82c84a88434
soltanoff <piccadillable@gmail.com>
2019-04-30 14:17:22 +0300

    index page improvements v0.1

--- blog_v2/static/js/main.js
+++ blog_v2/static/js/main.js
@@ -3,18 +3,43 @@ new Vue({
     delimiters: ['${','}'],
     data: {
         loading: false,
+        count_pages: 1,
+        current_page: 1,
+        next_page: null,
+        previous_page: null,
         articles: [],
+        tags: [],
     },
     mounted: function() {
-        this.getArticles();
+        this.getCurrentArticles();
     },
     methods: {
-        getArticles: function() {
+        getCurrentArticles: function() {
+            this.getArticlesByPage(this.current_page);
+        },
+        getArticlesByPage: function(page) {
+            this._getArticleTags();
             this.loading = true;
-            this.$http.get('/api/post/')
+            this.$http.get('/api/post/?page=' + page)
             .then((response) => {
                 this.loading = false;
-                this.articles = response.data;
+                this.current_page = page;
+                this.count_pages = response.data['count_pages'];
+                this.next_page = response.data['next_page'];
+                this.previous_page = response.data['previous_page'];
+                this.articles = response.data['results'];
+            })
+            .catch((err) => {
+                this.loading = false;
+                console.log(err);
+            })
+        },
+        _getArticleTags: function() {
+            this.loading = true;
+            this.$http.get('/api/post/tags/')
+            .then((response) => {
+                this.loading = false;
+                this.tags = response.data;
             })
             .catch((err) => {
                 this.loading = false;
